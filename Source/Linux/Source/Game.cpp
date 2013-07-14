d638255d7c894661a08022de0446f322c670e153
Rico <teh_programerer@interactivelaboratories.com>
2013-07-14 16:54:42 +0000

    |SOURCE| [Linux::Game] * Default contrustor is assigning sane values to the member variiables * Destructor is cleaning up heap allocated variables

--- Source/Linux/Source/Game.cpp
+++ Source/Linux/Source/Game.cpp
@@ -4,10 +4,33 @@ namespace UndeadAnimus
 {
 	Game::Game( )
 	{
+		m_pRenderer = ZED_NULL;
+		m_pConfigFile = ZED_NULL;
+		m_FullScreen = ZED_FALSE;
+		m_Running = ZED_FALSE;
+		m_pInputManager = ZED_NULL;
+		m_pWindow = ZED_NULL;
 	}
 
 	Game::~Game( )
 	{
+		if( m_pInputManager )
+		{
+			delete m_pInputManager;
+			m_pInputManager = ZED_NULL;
+		}
+
+		if( m_pRenderer )
+		{
+			delete m_pRenderer;
+			m_pRenderer = ZED_NULL;
+		}
+
+		if( m_pWindow )
+		{
+			delete m_pWindow;
+			m_pWindow = ZED_NULL;
+		}
 	}
 
 	ZED_UINT32 Game::Initialise( )
